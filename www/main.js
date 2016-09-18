b6d3a3b895dee96ba010e6abb98aff7f6fddfc37
Albert Z <albertzhang66@outlook.com>
2016-09-17 19:39:33 -0500

    Remove accountMetadata on error

--- www/main.js
+++ www/main.js
@@ -48,63 +48,63 @@ $(document).ready(function () {
         });
 
         // Initialize SecureStorage plugin and get metadata
-        ss = new cordova.plugins.SecureStorage(
-            function () { // If success
-                console.info("Secure storage init complete!");
-                ss.get(function (metadata) { // If success
-                    accountMetadata = JSON.parse(metadata).accounts;
-                    console.info("Successfully retrieved metadata!");
+        ss = new cordova.plugins.SecureStorage(function () { // If success
+            console.info("Secure storage init complete!");
+            ss.get(function (metadata) { // If success
+                accountMetadata = JSON.parse(metadata).accounts;
+                console.info("Successfully retrieved metadata!");
 
-                    loadAcctList();
+                loadAcctList();
 
-                    document.addEventListener("resume", onResume, false);
-                    document.addEventListener("backbutton", updateTitle, false);
+                document.addEventListener("resume", onResume, false);
+                document.addEventListener("backbutton", updateTitle, false);
 
-                    AppRate.preferences.displayAppName = "ScoreScope";
-                    AppRate.preferences.useLanguage = 'en';
-                    AppRate.preferences.storeAppURL = {
-                        ios: "com.albertzhang.scorescope",
-                        android: "market://details?id=com.albertzhang.scorescope",
-                        windows: "ms-windows-store://pdp/?ProductId=9NBLGGH515N6"
-                    };
-                    AppRate.promptForRating(false); // need false parameter to respect user decline
-                }, function (error) { // If there's an error, fuck it and make user log in again
-                    console.error(error);
-                    console.warn("Assuming first run! Redirecting...");
+                AppRate.preferences.displayAppName = "ScoreScope";
+                AppRate.preferences.useLanguage = 'en';
+                AppRate.preferences.storeAppURL = {
+                    ios: "com.albertzhang.scorescope",
+                    android: "market://details?id=com.albertzhang.scorescope",
+                    windows: "ms-windows-store://pdp/?ProductId=9NBLGGH515N6"
+                };
+                AppRate.promptForRating(false); // need false parameter to respect user decline
+            }, function (error) { // If there's an error in the initialization process, fuck it and make user log in again
+                console.error(error);
+                console.warn("Error in initialization process...making user log in again!");
+                ss.remove(function () { // On success
                     window.location.replace("accounts/firstRun.html");
+                }, function (error) { // On failure
+                    console.error(error);
                 }, "accountMetadata");
+            }, "accountMetadata");
 
-                // Retrieve settings as well
-                ss.get(function (settingsJSON) { // If success
-                    settings = JSON.parse(settingsJSON);
-                    console.info("Successfully retrieved settings!");
+            // Retrieve settings as well
+            ss.get(function (settingsJSON) { // If success
+                settings = JSON.parse(settingsJSON);
+                console.info("Successfully retrieved settings!");
 
-                    if (typeof settings.version === "undefined" || settings.version < VERSION) { // On upgrade
-                        console.info("Settings are out of date! Upgrading to " + VERSION);
+                if (typeof settings.version === "undefined" || settings.version < VERSION) { // On upgrade
+                    console.info("Settings are out of date! Upgrading to " + VERSION);
 
-                        // Merge settings with default settings, with settings having priority
-                        settings = $.extend(DEFAULT_SETTINGS, settings);
+                    // Merge settings with default settings, with settings having priority
+                    settings = $.extend(DEFAULT_SETTINGS, settings);
 
-                        ss.set(function (key) { // On success
-                            console.info("Successfully upgraded settings to " + VERSION);
-                        }, function (error) { // On error
-                            // Probably shouldn't bother notifying the user.
-                            console.warn("Failed to upgrade settings! Error: " + error);
-                            // TODO: Send error log
-                        }, "settings", JSON.stringify(settings));
-                    }
-                }, function (error) { // If there's an error, set settings to defaults
-                    console.warn(error);
-                    console.warn("Could not retrieve settings...resetting to defaults");
-                    saveSettings(DEFAULT_SETTINGS);
-                }, "settings");
-            },
-            function (error) {
-                console.error("Error: " + error);
-                displayErrorPage("#mainPageErrMsgDiv", "Oh No!", "Failed to initialize secure storage. Please try again later.", "ErrorTriangle", null);
-            },
-            "scorescope" // Key for storage.
-        );
+                    ss.set(function (key) { // On success
+                        console.info("Successfully upgraded settings to " + VERSION);
+                    }, function (error) { // On error
+                        // Probably shouldn't bother notifying the user.
+                        console.warn("Failed to upgrade settings! Error: " + error);
+                        // TODO: Send error log
+                    }, "settings", JSON.stringify(settings));
+                }
+            }, function (error) { // If there's an error, set settings to defaults
+                console.warn(error);
+                console.warn("Could not retrieve settings...resetting to defaults");
+                saveSettings(DEFAULT_SETTINGS);
+            }, "settings");
+        }, function (error) {
+            console.error("Error: " + error);
+            displayErrorPage("#mainPageErrMsgDiv", "Oh No!", "Failed to initialize secure storage. Please try again later.", "ErrorTriangle", null);
+        }, "scorescope"); // Key for storage
     });
 });
 
